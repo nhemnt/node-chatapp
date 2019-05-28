326d716d1e6872eff5e2e67af89504998dcbb8b5
hemant <nhemnt@gmail.com>
2019-05-29 00:50:06 +0530

    add test md\iddleware

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const app = express();
 
 app.set('port', process.env.port || 3000);
 
+let testMiddleware = (req, res, next) => {
+    req.hello = 'test middleware'
+    next();
+}
+app.use('/',testMiddleware);
+
 app.get('/', (req, res, next) => { 
     res.send("<h1> heello </h1>");
 })
 app.get('/dashboard', (req, res, next) => { 
-    res.send("<h1> heello dashboard</h1>");
+    res.send(`<h1> heello dashboard ${req.hello}</h1>`);
 })
 
 app.listen(app.get('port'), () => { 
