c23b66b020218aadd8b5354e974921110ad3862e
hemant <nhemnt@gmail.com>
2019-05-29 01:17:18 +0530

    add views

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const app = express();
 
 app.set('port', process.env.port || 3000);
 
-let testMiddleware = (req, res, next) => {
-    req.hello = 'test middleware'
-    next();
-}
-app.use('/',testMiddleware);
+//middleware
+// let testMiddleware = (req, res, next) => {
+//     req.hello = 'test middleware'
+//     next();
+// }
+// app.use('/',testMiddleware);
 
 app.get('/', (req, res, next) => { 
-    res.send("<h1> heello </h1>");
-})
-app.get('/dashboard', (req, res, next) => { 
-    res.send(`<h1> heello dashboard ${req.hello}</h1>`);
+    // res.send("<h1> heello </h1>");
+    res.sendFile(__dirname + "/views/login.htm")
 })
 
 app.listen(app.get('port'), () => { 
